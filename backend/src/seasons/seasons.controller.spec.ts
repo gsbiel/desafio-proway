f632d1058f4923a553a5285194cda945ff3a4984
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-09-25 15:49:14 -0300

    [seasons] use nestjs/Test class to implement testing

--- backend/src/seasons/seasons.controller.spec.ts
+++ backend/src/seasons/seasons.controller.spec.ts
@@ -1,22 +1,31 @@
 import { SeasonsController } from "./seasons.controller"
 import { SeasonsService } from "./seasons.service";
+import { Test } from "@nestjs/testing";
 
 describe('SeasonsController', () => {
     let seasonsController: SeasonsController;
     let seasonsService: SeasonsService;
 
-    beforeEach(() =>{
-        seasonsService = new SeasonsService()
-        seasonsController = new SeasonsController(seasonsService)
+    beforeEach(async () =>{
+        const moduleRef = await Test.createTestingModule({
+            controllers: [SeasonsController],
+            providers: [SeasonsService],
+        }).compile();
+
+        seasonsService = moduleRef.get<SeasonsService>(SeasonsService)
+        seasonsController = moduleRef.get<SeasonsController>(SeasonsController)
     })
 
     describe('findAllSeasons', () => {
 
         it('should return a feedback string', async () => {
-            const result = "Returning all seasons..."
+
+            const result = "Returning all seasons...."
+
             jest.spyOn(seasonsService, 'findAllSeasons').mockImplementation(() => {
                 return result
             })
+
             expect(await seasonsController.findAll()).toBe(result)
         })
     })
