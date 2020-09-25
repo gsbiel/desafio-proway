bc421ac027169cefdb26976616d8f2b142bc535c
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-09-25 16:05:51 -0300

    configure project to use typeORM and sqlite3

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import {Connection} from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SeasonsModule } from './seasons/seasons.module';
 
 @Module({
-  imports: [SeasonsModule],
+  imports: [
+    SeasonsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: '../database.sql',
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor( private connection: Connection) {}
+}
