9ff382facefbe2c371ca1df52fa5e499c43e44ee
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-09-25 20:45:49 -0300

    [users] implement get, create and delete user operations

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body } from "@nestjs/common";
+import { Controller, Get, Post, Body, Query, Delete } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./users.dto";
+import { User } from "src/entities/user.entity";
 
 
 @Controller('users')
@@ -9,13 +10,18 @@ export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Get()
-    findById(): String{
-        return this.usersService.findUserById()
+    async findById(@Query('id') id: string): Promise<User[]>{
+        return await this.usersService.findUserById(id)
     }
 
     @Post()
-    createUser(@Body() createUserDto: CreateUserDto): String {
-        return this.usersService.createUser(createUserDto)
+    async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+        return await this.usersService.createUser(createUserDto)
+    }
+
+    @Delete()
+    async deleteById(@Query('id') id: string){
+        return await this.usersService.deleteUserById(id)
     }
 
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,22 +1,74 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, HttpException, HttpStatus} from "@nestjs/common"
 import { CreateUserDto } from "./users.dto"
 import { InjectRepository } from "@nestjs/typeorm"
 import { User } from "src/entities/user.entity"
-import { Repository } from "typeorm"
+import { Repository} from "typeorm"
 
 @Injectable()
 export class UsersService {
 
     constructor(
         @InjectRepository(User)
-        private usersRepository: Repository<User>
+        private usersRepository: Repository<User>,
     ){}
 
-    findUserById(): String {
-        return "returning user..."
+    async findUserById(id: string): Promise<User[]> {
+        let user = null
+        if(id){
+            user = await this.usersRepository.find({ where: { id: id} });
+        }
+        else{
+            user = await this.usersRepository.find();
+        }
+        return user
     }
 
-    createUser(createUserDto: CreateUserDto): String {
-        return `creating user ${createUserDto.name}`
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+
+        // Checa se o usuário já existe
+        const userFound = await this.usersRepository.find({
+            where: [
+                { login : createUserDto.login },
+                { email : createUserDto.email }
+            ]
+        })
+        if(userFound.length > 0){
+
+            let errorMessage = ""
+
+            if(userFound[0].login == createUserDto.login){
+                errorMessage = "The user already exists."
+            }
+
+            if(userFound[0].email == createUserDto.email){
+                errorMessage += " The e-mail is already in use."
+            }
+
+            throw new HttpException({
+                status: HttpStatus.CONFLICT,
+                error: errorMessage
+            }, HttpStatus.CONFLICT);  
+        }
+
+        const user = new User()
+        user.name = createUserDto.name
+        user.email = createUserDto.email
+        user.login = createUserDto.login
+        user.password = createUserDto.password
+        await this.usersRepository.save(user)
+        return user
+    }
+
+    async deleteUserById(id: string){
+        
+        const userFound = await this.usersRepository.find({
+            where: {id:id}
+        })
+
+        if(userFound.length > 0){
+            return await this.usersRepository.delete(userFound[0])
+        }
+        
+        return {}
     }
 }
