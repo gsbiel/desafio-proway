ad82848c51f9ab2ec90ac0c624508810ceeda219
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-09-26 13:51:01 -0300

    [seasons] inject User repository into the module

--- backend/src/seasons/seasons.module.ts
+++ backend/src/seasons/seasons.module.ts
@@ -3,10 +3,11 @@ import { SeasonsController } from "./seasons.controller";
 import { SeasonsService } from "./seasons.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Season } from "src/entities/season.entity";
+import { User } from "src/entities/user.entity";
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Season])],
+    imports: [TypeOrmModule.forFeature([User,Season])],
     controllers: [SeasonsController],
     providers: [SeasonsService],
 })
