061a0fd79714d27e08d53852cd7c441434782502
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-09-27 14:22:57 -0300

    [app] implement signup route in controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { CreateUserDto } from './users/users.dto';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
   constructor(
+    private readonly usersService: UsersService,
     private readonly appService: AppService,
     private readonly authService: AuthService
     ) {}
@@ -26,5 +29,10 @@ export class AppController {
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
+  }
+
+  @Post('/signup')
+  async signUpUser(@Body() createUpserDto: CreateUserDto){
+    return await this.usersService.createUser(createUpserDto)
   }
 }
