df092530950053866f601b461da17abf6c28058f
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-09-27 21:19:54 -0300

    create theme colors pallete

---
+++ frontend/web/src/constants.ts
@@ -0,0 +1,12 @@
+export const colorTheme = {
+
+    primaryDark: "#58497a",
+    primary: "#8675a9",
+    primaryLight: "#b6a4db",
+
+    secondaryDark:"#927fa5",
+    secondary:"#c3aed6",
+    secondaryLight:"#f6e0ff",
+
+    textColor: "#ffff"
+}
