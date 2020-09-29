4d9fc99366c790bc51d35bf7dd1dd8acace2c53a
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-09-28 22:15:54 -0300

    create basic implementation of Painel component

---
+++ frontend/web/src/pages/painel/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    PainelContainer
+} from './styles';
+
+const Painel = () => {
+
+    return(
+        <PainelContainer>
+            <h1>Painel Aqui</h1>
+        </PainelContainer>
+    )
+}
---
+++ frontend/web/src/pages/painel/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const PainelContainer = styled.div`
+    width:100%;
+    height:100%;
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+`;
