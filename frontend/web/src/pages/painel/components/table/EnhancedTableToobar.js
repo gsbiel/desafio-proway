4d1e9069a7c640b446324829aa6ad47ef4f78197
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-09-29 05:33:24 -0300

    implement add and edit buttons for Season and Game tables

--- frontend/web/src/pages/painel/components/table/EnhancedTableToobar.js
+++ frontend/web/src/pages/painel/components/table/EnhancedTableToobar.js
@@ -7,6 +7,9 @@ import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
 import { lighten, makeStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import EditIcon from '@material-ui/icons/Edit';
 
 const useToolbarStyles = makeStyles((theme) => ({
     root: {
@@ -44,17 +47,31 @@ const EnhancedTableToolbar = (props) => {
           </Typography>
         ) : (
           <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
-              Seasons
+              {props.tablePath}
           </Typography>
         )}
   
         {numSelected > 0 ? (
-          <Tooltip title="Delete">
-            <IconButton aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
+          [
+            <Tooltip title="Delete">
+              <IconButton aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+            </Tooltip>,
+
+            <Tooltip title="Edit" aria-label="add">
+            <Fab color="secondary" className={classes.fab}>
+              <EditIcon />
+            </Fab>
+            </Tooltip>
+          ]
+        ) : 
+          <Tooltip title="Add" aria-label="add">
+            <Fab color="secondary" className={classes.fab}>
+              <AddIcon style={{ fontSize: 30}} />
+            </Fab>
           </Tooltip>
-        ) : null}
+        }
       </Toolbar>
     );
   };
