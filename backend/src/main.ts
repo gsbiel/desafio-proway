a66f5179b93e81ce2fcb632ff68a7284445f1a6b
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-09-30 00:24:36 -0300

    change port to avoid conflict and enable cors policy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({
     //disableErrorMessages: true,
   }))
-  await app.listen(3001);
+  await app.listen(3002);
 }
 
 bootstrap();
