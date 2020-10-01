30c82d6bc3d2054463e4fb83d6b73a7d8de11de5
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-10-01 01:14:17 -0300

    create and implement Bubble component

---
+++ frontend/web/src/components/bubble/Bubble.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {
+    BubbleContainer,
+} from './styles';
+
+import './bubble.css';
+
+const Bubble = () => {
+
+    return (
+        <BubbleContainer>
+            <div className="bubble x1"/>
+            <div className="bubble x2"/>
+            <div className="bubble x3"/>
+            <div className="bubble x4"/>
+            <div className="bubble x5"/>
+            <div className="bubble x6"/>
+            <div className="bubble x7"/>
+            <div className="bubble x8"/>
+            <div className="bubble x9"/>
+            <div className="bubble x10"/>
+        </BubbleContainer>
+    );
+}
+
+export default Bubble;
---
+++ frontend/web/src/components/bubble/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+
+export const BubbleContainer = styled.div`
+    width:100%;
+    height:100%;
+    opacity: 0.2;
+    display: absolute;
+    z-index:1;
+`;
+
+export const Bubble = styled.div``;
