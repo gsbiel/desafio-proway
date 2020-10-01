06f70822a5d16e133830daef370ce474f3a16623
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-10-01 02:08:09 -0300

    implement PainelHeader component

---
+++ frontend/web/src/pages/painel/components/painelHeader/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+    HeaderContainer,
+    HeaderTitle
+} from './styles';
+
+const PainelHeader = () => {
+
+    return(
+        <HeaderContainer>
+            <HeaderTitle>Records</HeaderTitle>
+        </HeaderContainer>
+    );
+};
+
+export default PainelHeader;
---
+++ frontend/web/src/pages/painel/components/painelHeader/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+    width: 100%;
+    height:80px;
+    background-color: rgba(88,73,122);
+    display: flex;
+    justify-content:center;
+    align-items:center;
+`;
+
+export const HeaderTitle = styled.span`
+    color: #ffff;
+    font-weight: bolder;
+    font-size: 24px;
+`;
