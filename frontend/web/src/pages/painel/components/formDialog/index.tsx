2416a33cbb0206093500f6022abe8a69702a61ea
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-10-02 14:58:04 -0300

    remove unecessary imports from LoginForm component

--- frontend/web/src/pages/painel/components/formDialog/index.tsx
+++ frontend/web/src/pages/painel/components/formDialog/index.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 
 import {RootState} from '../../../../index';
 import {painelCloseDialogueForm} from '../../../../store/actions/painel';
-import {DialogueFormModeType} from '../../../../store/reducers/painel';
 
 import CreateSeasonForm from './createSeasonForm';
 import CreateGameForm from './createGameForm';
-
-interface DialogueFormMode{
-  entity:string,
-  action:string
-}
 
 const FormDialog = () => {
 
